4855182622f9e45be53799f65ea7ce88d24631b8
Pedro Henrique <phmstulp@minha.fag.edu.br>
2020-10-27 21:25:05 -0300

    Cadastrando, Deletando e Consultando

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,117 +1,59 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import { useState, useEffect } from 'react';
-import Api from './Api';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
+import './App.css';
+import { makeStyles } from "@material-ui/core/styles"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import { Drawer, List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core"
+import Produto from './pages/produto';
+import ProdutoList from './pages/produto-list';
+import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 
-const columns = [
-    { id: 'acoes', label: 'Ações', minWidth: 100, align: 'center' },
-    { id: 'cdProduto', label: 'Código', minWidth: 200, align: 'left' },
-    { id: 'dsProduto', label: 'Descrição', minWidth: 200, align: 'left' },
-    { id: 'cdMarca', label: 'Marca', minWidth: 100, align: 'center' },
-    { id: 'dsObs', label: 'Observação', minWidth: 200, align: 'center' },
-    { id: 'nrValor', label: 'Valor', minWidth: 200, align: 'center' },
-];
-
-const useStyles = makeStyles({
-    root: {
-        width: '100%',
-    },
-    container: {
-        maxHeight: 440,
-    },
-});
-
-const Delete = (event) => {
-    Api.delete('produto/');
-    console.log('Deletou');
-}
-
-const Edit = (event) => {
-    console.log('Editou');
-    event.preventDefault();
-}
+const useStyles = makeStyles((theme) => ({
+  drawerPaper: { width: 'inherit' },
+  link: {
+    textDecoration: 'none',
+    color: theme.palette.text.primary
+  }
+}))
 
 function App() {
-    const classes = useStyles();
-    const [page, setPage] = React.useState(0);
-    const [data, setData] = useState([]);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-    useEffect(() => {
-        const GetData = async () => {
-          const response = await Api.get('produto');
-            setData(response.data);
-        }
-        GetData();
-        console.log(data);
-    }, []);
-
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
-
-    return (
-        <Paper className={classes.root}>
-            <TableContainer className={classes.container}>
-                <Table stickyHeader aria-label="sticky table">
-                    <TableHead>
-                        <TableRow>
-                            {columns.map((column) => (
-                                <TableCell
-                                    key={column.id}
-                                    align={column.align}
-                                    style={{ minWidth: column.minWidth }, { fontWeight: "bold" }}
-                                >
-                                    {column.label}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                            return (
-                                <TableRow >
-                                    <TableCell align="center">{row.acoes}
-                                      <DeleteIcon onClick={Delete}/>
-                                      <EditIcon onClick={Edit}/>
-                                    </TableCell>
-                                    <TableCell align="left">{row.cdProduto}</TableCell>
-                                    <TableCell align="left">{row.dsProduto}</TableCell>
-                                    <TableCell align="center">{row.cdMarca}</TableCell>
-                                    <TableCell align="center">{row.dsObs}</TableCell>
-                                    <TableCell align="center">{row.nrValor}</TableCell>
-                                </TableRow>
-                            );
-                        })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[5, 10, 15]}
-                component="div"
-                count={data.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onChangePage={handleChangePage}
-                onChangeRowsPerPage={handleChangeRowsPerPage}
-            />
-        </Paper>
-    );
+  const classes = useStyles()
+  return (
+    <Router>
+      <div style={{ display: 'flex' }}>
+        <Drawer
+          style={{ width: '220px' }}
+          variant="persistent"
+          anchor="left"
+          open={true}
+          classes={{ paper: classes.drawerPaper }}
+        >
+          <List>
+            <Link to="/produto-list" className= { classes.link }>
+              <ListItem>
+                <ListItemIcon button>
+                  <FormatListBulletedIcon/>
+                </ListItemIcon>
+                <ListItemText primary={ "Consulta de Produtos" }></ListItemText>
+              </ListItem>
+            </Link>
+            <Link to="/produto" className= { classes.link }>
+              <ListItem>
+                <ListItemIcon button>
+                  <AddCircleOutlineIcon/>
+                </ListItemIcon>
+                <ListItemText primary={ "Cadastro de Produto" }></ListItemText>
+              </ListItem>
+            </Link>
+          </List>
+        </Drawer>
+        <Switch>
+          <Route path="/produto-list" component={ ProdutoList } />
+          <Route path="/produto" component={ Produto } />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ Frontend/src/pages/produto.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import SaveIcon from '@material-ui/icons/Save';
+import { makeStyles } from '@material-ui/core/styles';
+import Api from '../Api';
+
+function Produto() {
+    const [cdProduto, setCdProduto] = React.useState("");
+    const [dsProduto, setDsProduto] = React.useState("");
+    const [cdMarca, setCdMarca] = React.useState("");
+    const [dsObs, setDsObs] = React.useState("");
+    const [nrValor, setNrValor] = React.useState("");
+
+    const useStyles = makeStyles((theme) => ({
+        button: {
+            margin: theme.spacing(1),
+        },
+    }));
+    const classes = useStyles();
+
+    async function Save(event) {
+        const next = await Api.get('produto/NextId');
+        var id = next.data;
+        const produto = {
+            "cdProduto": id,
+            "dsProduto": dsProduto,
+            "cdMarca": cdMarca,
+            "dsObs": dsObs,
+            "nrValor": nrValor
+        };
+
+        const respPost = await Api.post('produto', produto);
+    }
+
+    return (
+        <div>
+            <h1> Cadastro de Produto </h1>
+            <div>
+                <div>
+                    <TextField id="cdProduto" label="Código" value={cdProduto} onChange={e => setCdProduto(e.target.value)} required fullWidth />
+                </div>
+                <div>
+                    <TextField id="dsProduto" label="Descrição" value={dsProduto} onChange={e => setDsProduto(e.target.value)} required fullWidth />
+                </div>
+                <div>
+                    <TextField id="cdMarca" label="Marca" value={cdMarca} onChange={e => setCdMarca(e.target.value)} required fullWidth />
+                </div>
+                <div>
+                    <TextField id="dsObs" label="Observação" value={dsObs} onChange={e => setDsObs(e.target.value)} required fullWidth />
+                </div>
+                <div>
+                    <TextField id="nrValor" label="Valor" value={nrValor} onChange={e => setNrValor(e.target.value)} required fullWidth />
+                </div>
+            </div>
+            <div>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    size="large"
+                    className={classes.button}
+                    startIcon={<SaveIcon />}
+                    onClick={Save}>
+                    Salvar
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+export default Produto;
